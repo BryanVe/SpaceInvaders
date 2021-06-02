3d5c6db6924b4c14e3cfeec071003b255a4081f6
Anthony Luzquiños <sluzquinosa@uni.pe>
2021-06-02 00:53:14 -0500

    Giving love to the code

--- app/src/main/java/com/example/spaceinvaders/Bullet.kt
+++ app/src/main/java/com/example/spaceinvaders/Bullet.kt
@@ -2,49 +2,50 @@ package com.example.spaceinvaders
 
 import android.graphics.RectF
 
-class Bullet(screenY: Int,
-             private val speed: Float = 350f,
-             heightModifier: Float = 20f) {
+class Bullet(
+  screenY: Int,
+  private val speed: Float = 350f,
+  heightModifier: Float = 20f
+) {
 
-    val position = RectF()
+  val position = RectF()
 
-    // Which way is it shooting
-    val up = 0
-    val down = 1
+  // Which way is it shooting
+  val up = 0
+  val down = 1
 
-    // Going nowhere
-    private var heading = -1
+  // Going nowhere
+  private var heading = -1
 
-    private val width = 2
-    private var height = screenY / heightModifier
+  private val width = 2
+  private var height = screenY / heightModifier
 
-    var isActive = false
+  var isActive = false
 
-    fun shoot(startX: Float, startY: Float, direction: Int): Boolean {
-        if (!isActive) {
-            position.left = startX
-            position.top = startY
-            position.right = position.left + width
-            position.bottom = position.top + height
-            heading = direction
-            isActive = true
-            return true
-        }
+  fun shoot(startX: Float, startY: Float, direction: Int): Boolean {
+    if (!isActive) {
+      position.left = startX
+      position.top = startY
+      position.right = position.left + width
+      position.bottom = position.top + height
+      heading = direction
+      isActive = true
 
-        // Bullet already active
-        return false
+      return true
     }
 
-    fun update(fps: Long) {
+    // Bullet already active
+    return false
+  }
 
-        // Just move up or down
-        if (heading == up) {
-            position.top -= speed / fps
-        } else {
-            position.top += speed / fps
-        }
+  fun update(fps: Long) {
+    // Just move up or down
+    if (heading == up)
+      position.top -= speed / fps
+    else
+      position.top += speed / fps
 
-        // Update the bottom position
-        position.bottom = position.top + height
-    }
+    // Update the bottom position
+    position.bottom = position.top + height
+  }
 }
--- app/src/main/java/com/example/spaceinvaders/DefenceBrick.kt
+++ app/src/main/java/com/example/spaceinvaders/DefenceBrick.kt
@@ -4,26 +4,22 @@ import android.graphics.RectF
 
 
 class DefenceBrick(row: Int, column: Int, shelterNumber: Int, screenX: Int, screenY: Int) {
+  var isVisible = true
+  private val width = screenX / 180
+  private val height = screenY / 80
 
-    var isVisible = true
+  // Sometimes a bullet slips through this padding.
+  // Set padding to zero if this annoys you
+  private val brickPadding = 1
 
-    private val width = screenX / 180
-    private val height = screenY / 80
+  // The number of shelters
+  private val shelterPadding = screenX / 12f
+  private val startHeight = screenY - screenY / 10f * 2f
 
-    // Sometimes a bullet slips through this padding.
-    // Set padding to zero if this annoys you
-    private val brickPadding = 1
-
-    // The number of shelters
-    private val shelterPadding = screenX / 12f
-    private val startHeight = screenY - screenY / 10f * 2f
-
-    val position = RectF(column * width + brickPadding +
-            shelterPadding * shelterNumber +
-            shelterPadding + shelterPadding * shelterNumber,
-            row * height + brickPadding + startHeight,
-            column * width + width - brickPadding +
-                    shelterPadding * shelterNumber +
-                    shelterPadding + shelterPadding * shelterNumber,
-            row * height + height - brickPadding + startHeight)
+  val position = RectF(
+    column * width + brickPadding + shelterPadding * shelterNumber + shelterPadding + shelterPadding * shelterNumber,
+    row * height + brickPadding + startHeight,
+    column * width + width - brickPadding + shelterPadding * shelterNumber + shelterPadding + shelterPadding * shelterNumber,
+    row * height + height - brickPadding + startHeight
+  )
 }
